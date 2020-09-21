d4c00136c9c60fe4bb2bf24f0dd3663c3589de5f
Soldy <4786022+Soldy@users.noreply.github.com>
2020-09-21 14:04:23 +0100

    first version
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,76 @@
+const poolrcBase = (require('poolrc')).poolrc;
+const setupBase = (require('setuprc')).setupBase;
+const ic = new (require('interactiveConsole')).console();
+
+const consoleOut = function(name, log){
+    ic.printLn(
+        ic.style(
+            '['+name+']',
+            {color: 'green'}
+        ) + log
+    );
+};
+let defaultLevel = 5;
+let currentLevel = 5;
+const debugSubBase = function(nameIn){
+    this.log = function(log, level){
+        if(typeof level === 'undefined')
+            level = defaultLevel;
+        let time = (+new Date);
+        pool.add({
+               time,
+               log,
+               level
+        });
+        if(level > currentLevel)
+            consoleOut(name, log);
+    }
+    let name = nameIn;
+    let pool = new poolrcBase(4000);
+}
+
+const debugBase = function(settings){
+    this.newSub = function(name){
+        return newSub(name);
+    }
+    this.sub = function(name){
+        return agents[name];
+    }
+    this.link = function(name){
+        newSub(name);
+        return subs[name].log;
+    }
+    this.setup = function(){
+        return setup;
+    };
+    let newSub = function(name){
+        if(typeof subs[name] === 'undefined')
+            subs[name] = new debugSubBase();
+        return subs[name];
+    }
+    let setup = new setupBase({
+        'defaultLevel':{
+            'type'    : 'integer',
+            'min'     : 0,
+            'max'     : 0,
+            'default' : 5
+        },
+        'currentLevel':{
+            'type'    : 'integer',
+            'min'     : 0,
+            'max'     : 0,
+            'default' : 5
+        },
+        'consoleOut':{
+            'type'    :'function',
+            'default' :consoleOut
+        }
+    });
+    let subs = {};
+
+    if(typeof settings !== 'undefined')
+        setup.setup(settings);
+
+}
+
+exports.debugBase = debugBase;
